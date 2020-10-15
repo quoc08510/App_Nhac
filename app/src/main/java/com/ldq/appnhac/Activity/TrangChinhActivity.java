6f314f70bef3a4ee69f68a40cdb9d03bb680741d
quoc08510 <quoc08510@gmail.com>
2020-10-15 19:58:10 +0700

    chinh sua ngay 15/10/2020

--- app/src/main/java/com/ldq/appnhac/Activity/TrangChinhActivity.java
+++ app/src/main/java/com/ldq/appnhac/Activity/TrangChinhActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
 import com.ldq.appnhac.Adapter.MainViewPagerAdapter;
+import com.ldq.appnhac.Fragment.Fragment_Ho_So;
 import com.ldq.appnhac.Fragment.Fragment_Tim_Kiem;
 import com.ldq.appnhac.Fragment.Fragment_Trang_Chu;
 import com.ldq.appnhac.R;
@@ -27,10 +28,12 @@ public class TrangChinhActivity extends AppCompatActivity {
         MainViewPagerAdapter mainViewPagerAdapter= new MainViewPagerAdapter(getSupportFragmentManager());
         mainViewPagerAdapter.addFragment(new Fragment_Trang_Chu(),"trang chu");
         mainViewPagerAdapter.addFragment(new Fragment_Tim_Kiem(),"tim kiem");
+        mainViewPagerAdapter.addFragment(new Fragment_Ho_So(),"ho so");
         viewPager.setAdapter(mainViewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
         tabLayout.getTabAt(0).setIcon(R.drawable.icontrangchu);
         tabLayout.getTabAt(1).setIcon(R.drawable.iconsearch);
+        tabLayout.getTabAt(2).setIcon(R.drawable.iconhoso);
     }
 
     public void anhxa(){
