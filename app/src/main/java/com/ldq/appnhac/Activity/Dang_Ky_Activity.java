26b8a7c31e210cc456f8d66df17968badaab0d11
quoc08510 <quoc08510@gmail.com>
2020-10-15 12:04:20 +0700

    chinh sua ngay 15/10/2020

--- app/src/main/java/com/ldq/appnhac/Activity/Dang_Ky_Activity.java
+++ app/src/main/java/com/ldq/appnhac/Activity/Dang_Ky_Activity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.ldq.appnhac.Model.HoSo;
 import com.ldq.appnhac.Model.TaiKhoan;
 import com.ldq.appnhac.R;
 import com.ldq.appnhac.Server.APIServer;
@@ -34,24 +35,54 @@ public class Dang_Ky_Activity extends AppCompatActivity {
         btndangky.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String tentaikhoan = edtTenDangKy.getText().toString();
+                final String tentaikhoan = edtTenDangKy.getText().toString();
                 final Dataserver dataserver = APIServer.getServer();
-                Call<List<TaiKhoan>> callback = dataserver.getTaiKhoan(tentaikhoan);
+                final Call<List<TaiKhoan>> callback = dataserver.getTaiKhoan(tentaikhoan);
                 callback.enqueue(new Callback<List<TaiKhoan>>() {
                     @Override
                     public void onResponse(Call<List<TaiKhoan>> call, Response<List<TaiKhoan>> response) {
                         ArrayList<TaiKhoan> taiKhoan = (ArrayList<TaiKhoan>) response.body();
                         if (taiKhoan.size()==0){
                             if (edtmatkhau1.getText().toString().equals(edtmatkhau2.getText().toString())){
-                                Dataserver dataserver1 =APIServer.getServer();
-                                Call<String> callback = dataserver.getDangKyTaiKhoan(edtTenDangKy.getText().toString(),edtmatkhau1.getText().toString());
-                                callback.enqueue(new Callback<String>() {
+                                Call<String> callback1 = dataserver.getDangKyTaiKhoan(edtTenDangKy.getText().toString(),edtmatkhau1.getText().toString());
+                                callback1.enqueue(new Callback<String>() {
                                     @Override
                                     public void onResponse(Call<String> call, Response<String> response) {
                                         String ketqua = response.body();
                                         if (ketqua.equals("success")){
                                             arrayListtaikhoan.clear();
                                             Toast.makeText(Dang_Ky_Activity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                                            Call<List<TaiKhoan>> callback3 = dataserver.getTaiKhoan(tentaikhoan);
+                                            callback3.enqueue(new Callback<List<TaiKhoan>>() {
+                                                @Override
+                                                public void onResponse(Call<List<TaiKhoan>> call, Response<List<TaiKhoan>> response) {
+                                                    ArrayList<TaiKhoan> taiKhoans = (ArrayList<TaiKhoan>) response.body();
+                                                    if (taiKhoans.size()==1){
+                                                        Call<String> callback4 = dataserver.getDangKyHoSo(taiKhoans.get(0).getIdtaikhoan());
+                                                        callback4.enqueue(new Callback<String>() {
+                                                            @Override
+                                                            public void onResponse(Call<String> call, Response<String> response) {
+
+                                                            }
+
+                                                            @Override
+                                                            public void onFailure(Call<String> call, Throwable t) {
+
+                                                            }
+                                                        });
+                                                    }
+                                                }
+
+                                                @Override
+                                                public void onFailure(Call<List<TaiKhoan>> call, Throwable t) {
+
+                                                }
+                                            });
+                                            try {
+                                                Thread.sleep(2000);
+                                            } catch (InterruptedException e) {
+                                                e.printStackTrace();
+                                            }
                                             Intent intent = new Intent(Dang_Ky_Activity.this,MainActivity.class);
                                             arrayListtaikhoan.add(edtTenDangKy.getText().toString());
                                             arrayListtaikhoan.add(edtmatkhau1.getText().toString());
@@ -81,6 +112,8 @@ public class Dang_Ky_Activity extends AppCompatActivity {
 
                     }
                 });
+
+
             }
         });
 
